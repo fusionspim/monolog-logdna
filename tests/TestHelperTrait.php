de75be9cab51bb7c1a1404968c79489c5b452025
Jamie <ziadoz@gmail.com>
2019-06-03 13:14:07 +0100

    Add smart JSON formatter test case.

--- tests/TestHelperTrait.php
+++ tests/TestHelperTrait.php
@@ -2,7 +2,10 @@
 namespace Fusions\Test\Monolog\LogDna;
 
 use DateTime;
+use Exception;
 use Monolog\Logger;
+use stdClass;
+use Throwable;
 
 trait TestHelperTrait
 {
@@ -20,5 +23,43 @@ trait TestHelperTrait
             'datetime'   => DateTime::createFromFormat('U.u', sprintf('%.6F', microtime(true))),
             'extra'      => [],
         ];
+    }
+
+    public function getExceptionWithStackTrace(string $message = '', int $code = 0, Throwable $previous = null, array $trace = []): Exception
+    {
+        if (count($trace) === 0) {
+            $trace = [
+                [
+                    'class' => 'MyClass',
+                    'args'  => [true, false, 42, 42.42, 'FOO', ['FOO', 'BAR'], new stdClass],
+                    'type'  => '->',
+                    'file'  => '/my/fake/path/src/MyClass.php',
+                    'line'  => 256,
+                ],
+                [
+                    'class' => 'MyNestedClass',
+                    'args'  => ['FOO', 'BAR'],
+                    'type'  => '::',
+                    'file'  => '/my/fake/path/src/Nested/MyNestedClass.php',
+                    'line'  => 512,
+                ],
+                [
+                    'class' => 'MyVendorClass',
+                    'args'  => [],
+                    'type'  => '->',
+                    'file'  => '/my/fake/path/vendor/MyVendorClass.php',
+                    'line'  => 123,
+                ],
+                [
+                    'class'    => '',
+                    'args'     => [],
+                    'function' => 'require',
+                    'file'     => '/my/fake/path/require.php',
+                    'line'     => 42,
+                ]
+            ];
+        }
+
+        return new StackTraceTestException($message, $code, $previous, $trace);
     }
 }
