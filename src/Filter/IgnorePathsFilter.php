765d98cd5baf51778cf7cb692bc35fc55d57bed1
Jamie <ziadoz@gmail.com>
2021-02-10 14:04:56 +0000

    Remove extra space.

--- src/Filter/IgnorePathsFilter.php
+++ src/Filter/IgnorePathsFilter.php
@@ -7,7 +7,7 @@ class IgnorePathsFilter
 
     /**
      * Omit stack trace frames containing specific file paths.
-     * This is useful when you have deep stacks (e.g.  middleware) or want to exclude vendor components that aren't relevant.
+     * This is useful when you have deep stacks (e.g. middleware) or want to exclude vendor components that aren't relevant.
      * It also helps keep the JSON size down.
      *
      * Accepts an array of string file paths:
