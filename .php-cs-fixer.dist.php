2e0c8a6054c530d8cb2fdb3ae67b253f770b809e
Jamie <ziadoz@gmail.com>
2021-12-21 10:26:56 +0000

    Ignore Rector config.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,9 @@
 <?php
-return FusionsPim\PhpCsFixer\Factory::fromDefaults([
+$config = FusionsPim\PhpCsFixer\Factory::fromDefaults([
     'group_import' => false, // Currently broken for root classes
 ]);
+
+return $config->setFinder(
+    $config->getFinder()
+        ->notName('rector.php')
+);
